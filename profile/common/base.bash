1d829e3fb56486b627d6a55ca89b71fbc37f0d94
Cyano Hao <c@cyano.cn>
2024-05-06 07:26:25 +0800

    update binutils and gdb

--- profile/common/base.bash
+++ profile/common/base.bash
@@ -1,13 +1,13 @@
 export _REV="6"
 
-export _BINUTILS_VER="2.41"
+export _BINUTILS_VER="2.42"
 export _MINGW_VER="11.0.1"
 export _GCC_VER="13.2.0"
 export _GMP_VER="6.3.0"
 export _MPFR_VER="4.2.1"
 export _MPC_VER="1.3.1"
 export _ICONV_VER="1.17"
-export _GDB_VER="14.1"
+export _GDB_VER="14.2"
 export _MAKE_VER="4.4.1"
 
 export _UTF8_MANIFEST=1
