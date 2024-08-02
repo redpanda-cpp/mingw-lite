fcb5f7625502f546d5e319bbccee40824671dea0
Cyano Hao <c@cyano.cn>
2024-08-02 18:41:43 +0800

    upgrade to gcc 14.2

--- profile/common/base.bash
+++ profile/common/base.bash
@@ -1,15 +1,15 @@
-export _REV="4"
+export _REV="0"
 
 export _BINUTILS_VER="2.42"
 export _MINGW_VER="12.0.0"
-export _MCFGTHREAD_VER="1.8-ga"
-export _GCC_VER="14.1.0"
+export _MCFGTHREAD_VER="1.8-ga.3"
+export _GCC_VER="14.2.0"
 export _GMP_VER="6.3.0"
 export _MPFR_VER="4.2.1"
 export _MPC_VER="1.3.1"
 export _ICONV_VER="1.17"
 export _GETTEXT_VER="0.22.5"
-export _GDB_VER="14.2"
+export _GDB_VER="15.1"
 export _MAKE_VER="4.4.1"
 
 export _UTF8_MANIFEST=1
