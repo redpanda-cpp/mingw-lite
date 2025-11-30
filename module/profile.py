41f8504ab97a3ba6fc97a6e0752328e9b823f1b6
Cyano Hao <c@cyano.cn>
2025-12-01 07:16:03 +0800

    bump gcc to 16-20251130

--- module/profile.py
+++ module/profile.py
@@ -149,7 +149,7 @@ class BranchProfile(BranchVersions):
 
 BRANCHES: Dict[str, BranchVersions] = {
   '16': BranchVersions(
-    gcc = '16-20251123',
+    gcc = '16-20251130',
     rev = '0',
 
     mcfgthread = '2.2-ga.2',
