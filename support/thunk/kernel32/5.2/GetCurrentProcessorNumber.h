1facc2d5486e597d7809d0966d5660b68af53b91
Cyano Hao <c@cyano.cn>
2026-06-27 06:11:42 +0800

    update CPUID check in thunk

--- support/thunk/kernel32/5.2/GetCurrentProcessorNumber.h
+++ support/thunk/kernel32/5.2/GetCurrentProcessorNumber.h
@@ -7,5 +7,6 @@ namespace mingw_thunk
   namespace f
   {
     DWORD __stdcall fallback_GetCurrentProcessorNumber();
+    DWORD __stdcall zero_GetCurrentProcessorNumber();
   }
 } // namespace mingw_thunk
